ba57886af5cfc20aa3bc0626b4d1b90e37fd3c5e
nevean <neveangh@gmail.com>
2023-01-29 19:22:29 -0800

    our story page

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -5,15 +5,17 @@ const Story = () => {
     <>
       <div className="about-us">
         <img className="lavender" src="Lavender.jpg" alt="" />
-        <h3>Who we are</h3> <br />
-        <p>
-          One FLAWLESS skincare is a breakthrough, clean, clinically proven +
-          sustainable beauty brand with the ocean at its heart. Our formulas
-          fuse nature + science, using powerful marine ingredients from the
-          world’s oceans, sustainably produced with Blue Biotechnology to help
-          conserve our oceans. Our products are designed to do good for your
-          skin + give back to the ocean. <br /> Look Good. Feel Good. Do Good.
-        </p>
+        <div style={{ margin: "100px" }}>
+          <h3>Who we are</h3> <br />
+          <p>
+            Our FLAWLESS skincare is a breakthrough, clean, clinically proven +
+            sustainable beauty brand with the ocean at its heart. Our formulas
+            fuse nature + science, using powerful marine ingredients from the
+            world’s oceans, sustainably produced with Blue Biotechnology to help
+            conserve our oceans. Our products are designed to do good for your
+            skin + give back to the ocean. <br /> Look Good. Feel Good. Do Good.
+          </p>
+        </div>
       </div>
     </>
   );
