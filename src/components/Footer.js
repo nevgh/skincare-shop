17773d9c29c248a2cef99451235015af8c052a83
nevean <neveangh@gmail.com>
2023-01-31 00:28:03 -0800

    contact page update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,32 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <h4>Follow us</h4>
+      <div>
+        <h3>Customer Care</h3>
+        <p>Contact Us</p>
+        <p>1-888-442-5830</p>
+        <p>Track Your Order</p>
+        <p>Shipping & Delivery</p>
+      </div>
+
+      <div className="social-media">
+        <h4>Follow us</h4>
+        <img
+          src="./instagram.png"
+          alt="instagram"
+          style={{ width: "20px", margin: "5px" }}
+        />
+        <img
+          src="./tiktok.png"
+          alt="tiktok"
+          style={{ width: "20px", margin: "5px" }}
+        />
+        <img
+          src="./linkedin.png"
+          alt="linkedin"
+          style={{ width: "20px", margin: "5px" }}
+        />
+      </div>
     </div>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,23 +1,35 @@
-import React, {useRef} from "react";
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
-    console.log(
-      "sending an email"
-    )
+    console.log("sending an email");
     e.preventDefault();
 
-    emailjs.sendForm('service_otja1r9', 'template_4t5bg9i', form.current, 'B9vbike64FR1HSNGw')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_otja1r9",
+        "template_4t5bg9i",
+        form.current,
+        "B9vbike64FR1HSNGw"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+          alert("Message Sent");
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
+    e.target.reset();
   };
 
+  const handleSubmitClick = () => {
+    console.log(<p>Message Sent</p>);
+  };
   return (
     <div style={{ width: "40%", margin: "0 auto" }}>
       <div>
@@ -36,22 +48,23 @@ const Contact = () => {
         </p>
       </div>
       <div className="container">
-      <form ref={form} onSubmit={sendEmail}>
-        <fieldset>
-          <label for="name">Name</label>
-          <br />
-          <input type="text" name="name" id="name" />
-          <br />
-          <label for="email">Email</label> <br />
-          <input type="text" name="email" id="email" required />
-          <br />
-          <label for="message">Message</label>
-          <br />
-          <textarea type="text" name='message' ></textarea>
-          <br />
-          <button type="submit">Send</button>
-        </fieldset>
-
+        <form ref={form} onSubmit={sendEmail}>
+          <fieldset>
+            <label for="name">Name</label>
+            <br />
+            <input type="text" name="name" id="name" />
+            <br />
+            <label for="email">Email</label> <br />
+            <input type="text" name="email" id="email" required />
+            <br />
+            <label for="message">Message</label>
+            <br />
+            <textarea type="text" name="message"></textarea>
+            <br />
+            <button onSubmit={handleSubmitClick} type="submit">
+              Send
+            </button>
+          </fieldset>
         </form>
       </div>
     </div>
