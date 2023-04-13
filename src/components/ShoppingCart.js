f069ef80310a1936bcd1ef573da3df6a9f06ab22
nevean <neveangh@gmail.com>
2023-04-13 14:15:56 -0700

    fixed the issues inside cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -25,45 +25,60 @@ const ShoppingCart = ({
   }
 
   return (
-    <>
-      <div
-        style={{
-          position: "fixed",
-          top: 0,
-          bottom: 0,
-          right: isOpen ? 0 : -300,
-          width: 300,
-          height: "100%",
-          backgroundColor: "white",
-          border: ".5px grey",
-          transition: "right 0.3s ease-in-out",
-          zIndex: 1,
-          opacity: isOpen ? 1 : 0,
-        }}
-      >
-        <section id="cart-header">
-          <h3 style={{ paddingTop: "10px" }}>Your Cart</h3>
-          <button className="cart-x-button" onClick={() => setIsOpen(!isOpen)}>
-            x
-          </button>
-        </section>
-        <div style={{ padding: "10px" }}>
-          {cart.map((p) => (
-            <div key={p.id}>
-              <img className="cart-img" src={p.img} alt="products-images" />
-              <div className="product-info">
-                <h2 className="product-name">{p.name}</h2>
-                <p>$ {p.price}</p>
-                <button onClick={() => handleDecrementBtn(p)}> - </button>
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        bottom: 0,
+        right: isOpen ? 0 : -300,
+        width: 300,
+        height: "100%",
+        backgroundColor: "white",
+        border: ".5px grey",
+        transition: "right 0.3s ease-in-out",
+        zIndex: 1,
+        opacity: isOpen ? 1 : 0,
+      }}
+    >
+      <section id="cart-header">
+        <h3>Your Cart</h3>
+        <button className="cart-x-button" onClick={() => setIsOpen(!isOpen)}>
+          x
+        </button>
+      </section>
+      <div>
+        {cart.map((p) => (
+          <div className="cart-product-info" key={p.id}>
+            <img className="cart-img" src={p.img} alt="products-images" />
+            <div className="inside-product-info">
+              <h2 className="product-name">{p.name}</h2>
+              <span>$ {p.price}</span> <br></br>
+              <div className="cart-button-div">
+                <button
+                  className="inside-cart-button"
+                  onClick={() => handleDecrementBtn(p)}
+                >
+                  -
+                </button>
                 <span>{p.count}</span>
-                <button onClick={() => updateCart(p)}> + </button>
-                <button onClick={() => removeProductBtn(p)}>Remove</button>
+                <button
+                  className="inside-cart-button"
+                  onClick={() => updateCart(p)}
+                >
+                  +
+                </button>
+                <button
+                  className="inside-cart-button"
+                  onClick={() => removeProductBtn(p)}
+                >
+                  Remove
+                </button>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
